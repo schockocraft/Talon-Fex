954e66e65752fc458917b743ab5e42ed9c1cc8b7
* <leo.dantrimont@gmx.de>
2018-11-28 20:53:18 +0100

    add git update

--- commands/git.js
+++ commands/git.js
@@ -9,6 +9,14 @@ module.exports = (client, message, args) => {
   embed.setAuthor("schockocraft/Talon-Fex", "https://cdn.discordapp.com/attachments/498229013093810179/515203530051813409/1542904475253.png", "https://github.com/schockocraft/Talon-Fex/")
   embed.setColor(0x24292E)
   //message.respond(client.captions[client.guildsMeta[message.guild.id].language].git.main, embed)
-  message.respond(message.c.git.main, embed)
+ if (args[0] == "update") {
+ 	 if (permcheck(client, message, "all")) { //temporary local permission request, until permissions.js test for args
+ 	 message.delete()
+ 	 	 message.channel.send(message.c.commands.git.update, embed)
+ 	 	 }
+  }
+ else {
+  message.respond(message.c.commands.git.main, embed)
+  }
  
  } //module.exports
